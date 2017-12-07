a1b90c9fda68d6e120fe4b3d80ce76af1abbd047
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2017-12-07 15:37:26 +0100

    Task 5 add application with list of contacts using JSX

---
+++ components/App.js
@@ -0,0 +1,55 @@
+var contacts = [
+    {
+        id: 1,
+        firstName: 'Jan',
+        lastName: 'Nowak',
+        email: 'jan.nowak@example.com',
+    },
+    {
+        id: 2,
+        firstName: 'Adam',
+        lastName: 'Kowalski',
+        email: 'adam.kowalski@example.com',
+    },
+    {
+        id: 3,
+        firstName: 'Zbigniew',
+        lastName: 'Koziol',
+        email: 'zbigniew.koziol@example.com',
+    },
+    {
+        id: 4,
+        firstName: 'Anna',
+        lastName: 'Kowalska',
+        email: 'anna.kowalska@example.com',
+    },
+    {
+        id: 5,
+        firstName: 'Jan',
+        lastName: 'Marciniuk',
+        email: 'jan.marciniuk@example.com',
+    },
+    {
+        id: 6,
+        firstName: 'Karol',
+        lastName: 'Grabarski',
+        email: 'karol.grabarski@example.com',
+    }
+];
+      
+var contactForm = {
+    firstName: '',
+    lastName: '',
+    email: ''
+};
+
+      
+var App = React.createClass({
+    render: function() {
+        return 
+            <div className={'app'}>
+                React.createElement(ContactForm, {contact: contactForm}),
+                React.createElement(Contacts, {items: contacts}, {})
+            </div>
+    }
+});
